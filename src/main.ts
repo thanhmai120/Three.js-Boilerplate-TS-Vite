19addb91250614a1acc45472732e32a405fec4f9
thanhmai120 <thanhmai12061999@gmail.com>
2024-10-09 21:17:52 +0700

    Object 3D

--- src/main.ts
+++ src/main.ts
@@ -1,129 +1,106 @@
 import './style.css'
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
-// ____________SCENE
-// const sceneA = new THREE.Scene()
-// // scene.background = new THREE.Color(0x123456)
-// sceneA.background = new THREE.TextureLoader().load("https://sbcode.net/img/grid.png")
-
-// const sceneB = new THREE.Scene()
-// sceneB.background = new THREE.CubeTextureLoader()
-//                   .setPath('https://sbcode.net/img/')
-//                   .load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
-// sceneB.backgroundBlurriness = 0.3
-// const sceneC = new THREE.Scene()
-// sceneC.background = new THREE.Color(0xffe5ec)
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
-// _________CAMERA
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(0, 2, 4)
-camera.lookAt(0, 0.5, 0)
+camera.position.set(4, 4, 4)
 
-// const camera = new THREE.OrthographicCamera(-4, 4, 4, -4, -5, 10)
-// camera.position.set(1, 1, 1)
-// camera.lookAt(0, 0.5, 0)
-
-// ____RENDERER
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.render(scene, camera) // render whenever the screen size changes
-})
-
-// __________ORBIT CONTROL
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.addEventListener('change', () => {
-  renderer.render(scene, camera) // render whenever the OrbitControls changes
+controls.target.set(8, 0, 0)
+controls.update()
+
+const light = new THREE.PointLight(0xffffff, 400)
+light.position.set(10, 10, 10)
+scene.add(light)
+
+const object1 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0xff0000 }))
+object1.position.set(4, 0, 0)
+scene.add(object1)
+object1.add(new THREE.AxesHelper(5))
+
+const object2 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0x00ff00 }))
+object2.position.set(4, 0, 0)
+object1.add(object2)
+object2.add(new THREE.AxesHelper(5))
+
+const object3 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0x0000ff }))
+object3.position.set(4, 0, 0)
+object2.add(object3)
+object3.add(new THREE.AxesHelper(5))
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
 })
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshNormalMaterial({ wireframe: true })
-
-// ______CUBE
-const cube = new THREE.Mesh(geometry, material)
-cube.position.y = 0.5
-
-// ____________STAT
-const stats = new Stats()
-// stats.showPanel(1)
-document.body.appendChild(stats.dom)
-
-// _________ ADD BTNs SCENES
-// let activeScene = sceneC
-// const setScene = {
-//   sceneA: () => {
-//     activeScene = sceneA
-//   },
-//   sceneB: () => {
-//     activeScene = sceneB
-//   },
-//   sceneC: () => {
-//     activeScene = sceneC
-//   },
-// }
 
 const gui = new GUI()
-// gui.add(setScene, 'sceneA').name('Scene A')
-// gui.add(setScene, 'sceneB').name('Scene B')
-// gui.add(setScene, 'sceneC').name('Scene C')
+const object1Folder = gui.addFolder('Object1 (Red Ball)')
+object1Folder.add(object1.position, 'x', 0, 10, 0.01).name('X Position')
+object1Folder.add(object1.rotation, 'x', 0, Math.PI * 2, 0.01).name('X Rotation')
+object1Folder.add(object1.scale, 'x', 0, 2, 0.01).name('X Scale')
+object1Folder.open()
+const object2Folder = gui.addFolder('Object2 (Green Ball)')
+object2Folder.add(object2.position, 'x', 0, 10, 0.01).name('X Position')
+object2Folder.add(object2.rotation, 'x', 0, Math.PI * 2, 0.01).name('X Rotation')
+object2Folder.add(object2.scale, 'x', 0, 2, 0.01).name('X Scale')
+object2Folder.open()
+const object3Folder = gui.addFolder('Object3 (Blue Ball)')
+object3Folder.add(object3.position, 'x', 0, 10, 0.01).name('X Position')
+object3Folder.add(object3.rotation, 'x', 0, Math.PI * 2, 0.01).name('X Rotation')
+object3Folder.add(object3.scale, 'x', 0, 2, 0.01).name('X Scale')
+object3Folder.open()
 
-const cube_folder = gui.addFolder("Cube")
-cube_folder.add(cube.position, "y", 0, 20)
-const cam_folder = gui.addFolder("Camera")
-cam_folder.add(camera, "fov", 0, 180, 0.01).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cam_folder.add(camera, "aspect", 0.0001, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cam_folder.add(camera, "near", 0.001, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cam_folder.add(camera, "far", 0.001, 10).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-// cam_folder.add(camera, 'left', -10, 0).onChange(() => {
-//   camera.updateProjectionMatrix()
-// })
-// cam_folder.add(camera, 'right', 0, 10).onChange(() => {
-//   camera.updateProjectionMatrix()
-// })
-// cam_folder.add(camera, 'top', 0, 10).onChange(() => {
-//   camera.updateProjectionMatrix()
-// })
-// cam_folder.add(camera, 'bottom', -10, 0).onChange(() => {
-//   camera.updateProjectionMatrix()
-// })
-// cam_folder.add(camera, 'near', -5, 5).onChange(() => {
-//   camera.updateProjectionMatrix()
-// })
-// cam_folder.add(camera, 'far', 0, 10).onChange(() => {
-//   camera.updateProjectionMatrix()
-// })
-cam_folder.open()
-cam_folder.add(camera.position, 'x', 0, 20)
-cam_folder.add(camera.position, 'y', 0, 20)
-cam_folder.add(camera.position, 'z', 0, 20)
-// function animate() {
-//   requestAnimationFrame(animate)
-//   // stats.begin()
-//   // cube.rotation.x += 0.01
-//   // cube.rotation.y += 0.01
-//   // stats.end()
-//   activeScene.add(cube)
-//   activeScene.add(new THREE.GridHelper())
-//   renderer.render(activeScene, camera)
-//   stats.update()
-// }
+const stats = new Stats()
+document.body.appendChild(stats.dom)
 
-// animate()
-scene.add(cube)
-renderer.render(scene, camera)
+const debug = document.getElementById('debug') as HTMLDivElement
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    renderer.render(scene, camera)
+
+    const object1WorldPosition = new THREE.Vector3()
+    object1.getWorldPosition(object1WorldPosition)
+    const object2WorldPosition = new THREE.Vector3()
+    object2.getWorldPosition(object2WorldPosition)
+    const object3WorldPosition = new THREE.Vector3()
+    object3.getWorldPosition(object3WorldPosition)
+
+    debug.innerText =
+        'Red\n' +
+        'Local Pos X : ' +
+        object1.position.x.toFixed(2) +
+        '\n' +
+        'World Pos X : ' +
+        object1WorldPosition.x.toFixed(2) +
+        '\n' +
+        '\nGreen\n' +
+        'Local Pos X : ' +
+        object2.position.x.toFixed(2) +
+        '\n' +
+        'World Pos X : ' +
+        object2WorldPosition.x.toFixed(2) +
+        '\n' +
+        '\nBlue\n' +
+        'Local Pos X : ' +
+        object3.position.x.toFixed(2) +
+        '\n' +
+        'World Pos X : ' +
+        object3WorldPosition.x.toFixed(2) +
+        '\n'
+
+    stats.update()
+}
+
+animate()
